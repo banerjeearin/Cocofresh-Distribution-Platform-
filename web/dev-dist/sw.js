7e96e1b8391bd62470e9a6d5d0c4cdc9edbb82e5
Arindam Banerjee <Arindam.Banerjee@ril.com>
2026-04-08 22:14:29 +0530

    chore: sync latest changes

--- web/dev-dist/sw.js
+++ web/dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-6fc00345'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.noud3mca7ik"
+    "revision": "0.95a16f5746g"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
